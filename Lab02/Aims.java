065b607071d267e32271b164ccafdd7c890d095e
DungKDKT <Dung.NM2416682@sis.hust.edu.vn>
2025-10-10 07:11:22 +0700

    Update Aims.java

--- Lab02/Aims.java
+++ Lab02/Aims.java
@@ -3,14 +3,22 @@ package Aim;
 public class Aims {
 	public static void main(String[] args) {
 		Cart anOrder = new Cart();
-		DigitalVideoDisk dvd1 = new DigitalVideoDisk("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+		DigitalVideoDisk dvd1 = new DigitalVideoDisk("The Lion King", "Animation", "Roger Allers", 87, 20.00f);
 		anOrder.addDigitalVideoDisc(dvd1);
-		DigitalVideoDisk dvd2 = new DigitalVideoDisk("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+		DigitalVideoDisk dvd2 = new DigitalVideoDisk("Star Wars", "Science Fiction", "George Lucas", 87, 25.00f);
 		anOrder.addDigitalVideoDisc(dvd2);
-		DigitalVideoDisk dvd3 = new DigitalVideoDisk("Aladin", "Animation", 18.99f);
+		DigitalVideoDisk dvd3 = new DigitalVideoDisk("Aladin", "Animation", 19.00f);
 		anOrder.addDigitalVideoDisc(dvd3);
 		
 		System.out.println("Total cost is: ");
 		System.out.println(anOrder.totalCost());
+		
+	    System.out.println("\nRemoving 'Star Wars' from cart...");
+	    anOrder.removeDigitalVideoDisc(dvd2);
+	    System.out.println("Total cost after removal: ");
+	    System.out.println(anOrder.totalCost());
+	    
+	    System.out.println("\nTrying to remove 'Star Wars' again...");
+	    anOrder.removeDigitalVideoDisc(dvd2);
 	}
 }
