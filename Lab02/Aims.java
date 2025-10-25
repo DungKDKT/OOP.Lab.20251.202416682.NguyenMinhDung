a1ade567a98fb354c85446e540b81f96bc040ac0
DungKDKT <Dung.NM2416682@sis.hust.edu.vn>
2025-10-25 17:36:55 +0700

    Update Aims.java

--- Lab02/Aims.java
+++ Lab02/Aims.java
@@ -1,24 +1,27 @@
-package Aim;
+package hust.soict.dsai.aims.Aims;
+
+import hust.soict.dsai.aims.cart.Cart.Cart;
+import hust.soict.dsai.aims.disc.DigitalVideoDisk.DigitalVideoDisk;
 
 public class Aims {
 	public static void main(String[] args) {
 		Cart anOrder = new Cart();
 		DigitalVideoDisk dvd1 = new DigitalVideoDisk("The Lion King", "Animation", "Roger Allers", 87, 20.00f);
-		anOrder.addDigitalVideoDisc(dvd1);
+		anOrder.addDigitalVideoDisk(dvd1);
 		DigitalVideoDisk dvd2 = new DigitalVideoDisk("Star Wars", "Science Fiction", "George Lucas", 87, 25.00f);
-		anOrder.addDigitalVideoDisc(dvd2);
+		anOrder.addDigitalVideoDisk(dvd2);
 		DigitalVideoDisk dvd3 = new DigitalVideoDisk("Aladin", "Animation", 19.00f);
-		anOrder.addDigitalVideoDisc(dvd3);
+		anOrder.addDigitalVideoDisk(dvd3);
 		
 		System.out.println("Total cost is: ");
 		System.out.println(anOrder.totalCost());
 		
-	    System.out.println("\nRemoving 'Star Wars' from cart...");
-	    anOrder.removeDigitalVideoDisc(dvd2);
+		System.out.println("\nRemoving 'Star Wars' from cart...");
+	    anOrder.removeDigitalVideoDisk(dvd2);
 	    System.out.println("Total cost after removal: ");
 	    System.out.println(anOrder.totalCost());
-	    
-	    System.out.println("\nTrying to remove 'Star Wars' again...");
-	    anOrder.removeDigitalVideoDisc(dvd2);
+		    
+		System.out.println("\nTrying to remove 'Star Wars' again...");
+	    anOrder.removeDigitalVideoDisk(dvd2);
+		}
 	}
-}
